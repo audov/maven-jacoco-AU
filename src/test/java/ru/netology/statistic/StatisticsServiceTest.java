4763eac9cd260d31e607211eb31da210d472ff53
Neto test <audov@mail.ru>
2023-05-25 17:49:42 +0300

    tests changed to origin

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,8 +9,8 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
-        long expected = 13  ;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12  ;
 
         long actual = service.findMax(incomesInBillions);
 
